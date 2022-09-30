d57c105e899e58ad77903846a902e2b389b30990
mpraes <renan.de.moraes777@gmail.com>
2022-09-30 16:46:07 -0300

    Acrescentando código de cálculo do INSS e uma frase no código de local para printar

---
+++ calculo_inss.py
@@ -0,0 +1,8 @@
+
+salario = float(input('Digite Seu salário bruto CLT: '))
+
+inss = float(input('Informe seu percentual de INSS: '))
+
+valor_inss = salario * inss
+
+print("O valor de encargo INSS mensal é: " + str(valor_inss))
--- local.py
+++ local.py
@@ -2,4 +2,4 @@ endereco = input("Digite seu endereco: ")
 bairro = input("Digite seu bairro: ")
 cidade = input("Digite sua cidade: ")
 
-print(endereco + bairro + cidade)
+print("Você mora no endereço: " + endereco + bairro + cidade)
