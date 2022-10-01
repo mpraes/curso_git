e57b44e762431ffe54ac3b0a47e835b6242598a3
mpraes <renan.de.moraes777@gmail.com>
2022-10-01 06:58:53 -0300

    Alteração no print do arquivo 'local.py'

--- local.py
+++ local.py
@@ -1,5 +1,6 @@
 endereco = input("Digite seu endereco: ")
 bairro = input("Digite seu bairro: ")
 cidade = input("Digite sua cidade: ")
+estado = input("Digite seu estado: ")
 
-print("Você mora no endereço: " + endereco + bairro + cidade)
+print("Você mora no endereço: " + endereco + ". "+ bairro +". "+ cidade +". "+ estado)
