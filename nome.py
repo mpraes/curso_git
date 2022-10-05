eb2efbf6a2cffc54dbfb76242daf31406c14cdfd
mpraes <renan.de.moraes777@gmail.com>
2022-10-05 16:49:45 -0300

    Script de nome.py com novos campos inseridos

--- nome.py
+++ nome.py
@@ -1,5 +1,9 @@
 nome = input("Digite seu nome: ")
 cpf = input("Digite seu cpf: ")
 idade = input("Digite sua idade: ")
+sexo = input("Digite seu sexo: ")
+estadocivil = input("Digite seu estado civil: ")
 
-print("Meu nome e: "+ nome + ", meu cpf e "+ cpf + ". Tenho " + idade + " anos.")
+
+
+print("Meu nome é: "+ nome + ", meu cpf é "+ cpf + ". Tenho " + idade + " anos." + " Sexo "+ sexo + " e meu estado civil é " + estadocivil )
