ae3270613d65155877d2067a1bab94380812e018
mpraes <renan.de.moraes777@gmail.com>
2022-10-16 05:54:10 -0300

    Nova funcionalidade de cadastro de empresa

---
+++ novo_cadastro.py
@@ -0,0 +1,17 @@
+
+cnpjempresa = input("informe CNPJ da empresa (somente numeros): ")
+
+razaosocial = input("Informe razao social: ")
+
+telefonecontato = input("informe numero de telefone para contato: ")
+
+emailcontato = input("informe e-mail para contato: ")
+
+pessoacontato = input("informe nome do vendedor de contato: ")
+
+print(
+    cnpjempresa + ". \n" +
+    razaosocial + ". \n" +
+    telefonecontato + ". \n" +
+    emailcontato + ". \n" +
+    pessoacontato + ". \n")
