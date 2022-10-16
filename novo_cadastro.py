35598b71e3ae406d2d333d26b8784155fce186fb
mpraes <renan.de.moraes777@gmail.com>
2022-10-16 06:09:40 -0300

    Acrescentando mais variaveis no arquivo novo_cadastro

--- novo_cadastro.py
+++ novo_cadastro.py
@@ -9,9 +9,21 @@ emailcontato = input("informe e-mail para contato: ")
 
 pessoacontato = input("informe nome do vendedor de contato: ")
 
+materiaisouservicos1 = input("informe primeira palavra-chave de materiais/servicos: ")
+materiaisouservicos2 = input("informe segunda palavra-chave de materiais/servicos: ")
+materiaisouservicos3 = input("informe terceira palavra-chave de materiais/servicos: ")
+materiaisouservicos4 = input("informe quarta palavra-chave de materiais/servicos: ")
+materiaisouservicos5 = input("informe quinta palavra-chave de materiais/servicos: ")
+
+
 print(
     cnpjempresa + ". \n" +
     razaosocial + ". \n" +
     telefonecontato + ". \n" +
     emailcontato + ". \n" +
-    pessoacontato + ". \n")
+    pessoacontato + ". \n" +
+    materiaisouservicos1 + ". \n" +
+    materiaisouservicos2 + ". \n" +
+    materiaisouservicos3 + ". \n" +
+    materiaisouservicos4 + ". \n" +
+    materiaisouservicos5 + ". \n")
