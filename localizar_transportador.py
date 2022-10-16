b0cb81974544cbfd0cd501444dc296fe8c6bc5db
mpraes <renan.de.moraes777@gmail.com>
2022-10-16 09:59:44 -0300

    Criando nova funcionalidade de localizar transportador

---
+++ localizar_transportador.py
@@ -0,0 +1,11 @@
+
+transportadora = int(input("Digite CNPJ somente numeros ou Nome da Transportadora: "))
+
+transportadora_lista = {123468345000234:'TRANSPORTADORA SIMOES LTDA',523468345000255: 'FRETES DO HABACUQUE', 673468345000278: 'ABX EXPRESS'}
+
+for transportadora in transportadora_lista:
+    if transportadora in transportadora_lista:
+        print("Transportadora Encontrada")
+        break
+    else:
+        print('Transportadora nao encontrada. tente novamente ou verifique sua base')
