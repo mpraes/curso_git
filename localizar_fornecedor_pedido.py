12d74970adb568f91ffd9f34b101f4c1663787fc
mpraes <renan.de.moraes777@gmail.com>
2022-10-16 09:34:34 -0300

    Incluindo funcionalidade de localizar fornecedor

---
+++ localizar_fornecedor_pedido.py
@@ -0,0 +1,11 @@
+
+fornecedor = int(input("Digite codigo do Fornecedor: "))
+
+listafornecedores = [134,109,140,156]
+
+if fornecedor in listafornecedores:
+    print('Fornecedor encontrado.')
+    pedido_fornecedor = fornecedor
+else:
+    print("Fornecedor não encontrado, tente novamente ou verifique sua base de dados")
+
